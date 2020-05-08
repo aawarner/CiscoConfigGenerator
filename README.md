59d14e6161f1333b3d4a869ab4e008ed50467f7a
Aaron Warner <aawarner@cisco.com>
2020-05-08 14:16:51 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Cisco Configuration Generator
+A sample Cisco IOS-XE configuration tool. This tool can produce IOS-XE
+configurations by filling out a web form. 
+
+## Usage
+```
+python web.py
+``` 
+Once the web server has started open a web browser and proceed to 
+127.0.0.1:5000.
+
+The following web page will be displayed.
+
+*Insert Webpage photo here
+
+Once all pertinent information has been filled in click submit.
+The configuration will be found in the ~/configs directory.
+
+## Changing the Configuration Template
+The configuration template is a Jinja2 template with the file
+name ios-xe_template.j2. This template can be
+modified to include additional static configuration or variable
+configuration per individual need. User input from index.html is used
+in the config function of web.py to assign the user input to variables
+in ios-xe_config.j2 template. 
+
--- web.py
+++ web.py
@@ -1,3 +1,22 @@
+"""
+Copyright (c) 2019 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+Filename: config.py
+Version: Python 3.7.2
+Authors: Aaron Warner (aawarner@cisco.com)
+Description:    Simple IOS-XE configuration Generator. Run program and browse to 127.0.0.1:5000
+"""
+
 import jinja2
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash, send_file
